25b50f6a392a43f70b9bea29571b3a81e248bef3
Carolina Marques <carolina.marques@ifood.com.br>
2024-06-12 11:48:51 -0300

    Primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('caru.teste@teste.com')
+        cy.get('#password').type('abc123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, caru.teste (não é caru.teste? Sair)')
+    })
+
+})
